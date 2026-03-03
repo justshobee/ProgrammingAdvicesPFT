b586d2d202fce65c42c083138b44c91948dbc141
shobeedev <moha.saoud22@gmail.com>
2026-03-03 13:59:59 +0000

    Read Client Data and Convert it to one line

--- Algorithms & Problem-Solving Level 2/Problem#43.cpp
+++ Algorithms & Problem-Solving Level 2/Problem#43.cpp
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/03/03 10:43:17 by shobeedev               shobee4ever      */
-/*   Updated: 2026/03/03 11:19:54 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/03/03 13:59:00 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ struct stInfoClient
 	string PinCode = "";
 	string NameCl = "";
 	string PhoneCl = "";
-	string AccBalance = "";
+	double AccBalance;
 };
 
 stInfoClient ReadInfoClient()
@@ -43,6 +43,38 @@ stInfoClient ReadInfoClient()
 
 	return info;
 }
+void swap(char &a,char &b)
+{
+	char tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+string ReverseStr(string str)
+{
+	int i = 0;
+	int len = str.size();
+	while(i < len / 2)
+	{
+		swap(str[i],str[len-i-1]);
+		i++;
+	}
+	return str;
+}
+string Convertnbrtostr(int number)
+{
+	string word ="";
+	int rem = 0;
+	while(number != 0)
+	{
+		rem = number % 10;
+		number = number / 10;
+		char letter = rem + '0';
+		word += letter;
+	}
+	string str = ReverseStr(word);
+	return str;
+}
 string ConvertToLine(stInfoClient info,string sep = "#//#")
 {
 	string RecordLine = "";
@@ -51,7 +83,7 @@ string ConvertToLine(stInfoClient info,string sep = "#//#")
 	RecordLine += info.PinCode + sep;
 	RecordLine += info.NameCl + sep;
 	RecordLine += info.PhoneCl + sep;
-	RecordLine += info.AccBalance;
+	RecordLine += Convertnbrtostr(info.AccBalance);
 
 	return RecordLine;
 }
